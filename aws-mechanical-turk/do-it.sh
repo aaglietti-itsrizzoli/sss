31b958355bf5eb997cce1be21aedec02e43ac6eb
aaglietti-itsrizzoli <104371332+aaglietti-itsrizzoli@users.noreply.github.com>
2024-07-24 15:18:38 +0000

    feat(mech-turk): video download

--- aws-mechanical-turk/do-it.sh
+++ aws-mechanical-turk/do-it.sh
@@ -10,6 +10,21 @@ echo "#### ffmpeg version END"
 
 echo "yt-dlp --version: $(yt-dlp --version)"
 
-me=$(basename "$0")
+ME=$(basename "$0")
 
-echo "#### do-it.sh: $(date) - me is $(me)"
+echo "#### do-it.sh: $(date) - me is ${ME}"
+
+echo "#### Insert the football player name"
+read FOOTBALL_PLAYER_NAME
+echo "#### FOOTBALL_PLAYER_NAME: ${FOOTBALL_PLAYER_NAME}"
+
+echo "#### Insert the YouTube video URL"
+read YOUTUBE_VIDEO_URL
+
+echo "#### YOUTUBE_VIDEO_URL: ${YOUTUBE_VIDEO_URL}"
+YOUTUBE_VIDEO_FILENAME=tmp
+
+# yt-dlp --format=mp4 --output=$YOUTUBE_VIDEO_FILENAME https://www.youtube.com/watch?v=2ZEA7O3qdnE
+yt-dlp --format=mp4 --output=$YOUTUBE_VIDEO_FILENAME $YOUTUBE_VIDEO_URL
+
+rm $YOUTUBE_VIDEO_FILENAME
