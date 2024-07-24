d67c1002069ce1d8456cc13b09b788b8627f3ab4
aaglietti-itsrizzoli <104371332+aaglietti-itsrizzoli@users.noreply.github.com>
2024-07-24 23:21:58 +0000

    fix(seek): frame component

--- app/games/[gameId]/seek/[seekId]/page.tsx
+++ app/games/[gameId]/seek/[seekId]/page.tsx
@@ -2,15 +2,36 @@
 import { useEffect, useState } from "react"
 import Image from 'next/image'
 
+function Frame({ framePath, frameIndex, zIndex }: { framePath: string, frameIndex: number, zIndex: number }): JSX.Element {
+  return (
+    <Image
+      src={framePath}
+      width={500}
+      height={500}
+      alt="Picture of the author"
+      onClick={() => {
+      }}
+      style={{
+        position: 'absolute',
+        zIndex: zIndex
+      }}
+    />
+  )
+}
+
 export default function SeekById({ params }: { params: { seekId: string } }): JSX.Element {
-  const [data, setData] = useState([""])
+  const [data, setData] = useState([])
   const [isLoading, setLoading] = useState(true)
 
   useEffect(() => {
     fetch(`/pics/frames/${params.seekId}.json`)
       .then((res) => res.json())
       .then((data) => {
-        setData(data.frames)
+        setData(data.frames.map((framePath: string, frameIndex: number) => ({
+          framePath: `/${framePath}`,
+          frameIndex,
+          zIndex: data.frames.length - frameIndex
+        })))
         setLoading(false)
       })
   }, [params.seekId])
@@ -19,15 +40,17 @@ export default function SeekById({ params }: { params: { seekId: string } }): JS
   if (!data) return <p>No profile data</p>
 
   return (
-    <div>
-      {data.map(frame => (
-        <Image
-          src={`/${frame}`}
-          key={frame}
-          width={500}
-          height={500}
-          alt="Picture of the author"
-        />))}
+    <div style={{
+      position: 'relative'
+    }}>
+      {data.map((f: { framePath: string; frameIndex: number, zIndex: number }) => (
+        <Frame
+          framePath={f.framePath}
+          frameIndex={f.frameIndex}
+          zIndex={f.zIndex}
+          key={f.frameIndex}
+        />
+      ))}
     </div>
   )
 }
