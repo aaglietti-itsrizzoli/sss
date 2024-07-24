b33a1e500955e855d71712121bb4c80404c93751
aaglietti-itsrizzoli <104371332+aaglietti-itsrizzoli@users.noreply.github.com>
2024-07-24 12:22:51 +0000

    chore(devcontainer): postgresql tools

--- .devcontainer/post-create-command.sh
+++ .devcontainer/post-create-command.sh
@@ -10,5 +10,6 @@ me=$(basename "$0")
 
 echo "#### post-create-command.sh: $(date) - me is $(me)"
 
+sed -i "s#POSTGRES_URL#${POSTGRES_URL}#g" .vscode/settings.json
 
 echo "#### post-create-command.sh: $(date) - end"
