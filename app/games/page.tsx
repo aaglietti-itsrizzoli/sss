4a33d670b287832a1442f8c9edc4d6f79b1f2e62
aaglietti-itsrizzoli <104371332+aaglietti-itsrizzoli@users.noreply.github.com>
2024-07-24 17:17:04 +0000

    feat(seek): scaffold

--- app/games/page.tsx
+++ app/games/page.tsx
@@ -1,37 +1,28 @@
 import Link from "next/link";
 
-export default function Home() {
+export default async function Seek(): Promise<JSX.Element> {
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          SEEK SET SEND!
+          Seek!
         </p>
+        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
+        </div>
+        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
+        </div>
       </div>
-
       <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <Link href="/profile">
+        <Link href="/games/550e8400-e29b-41d4-a716-446655440000/seek/5c7a4a">
           <h2 className="mb-3 text-2xl font-semibold">
-            Profile{" "}
+            Game 550e8400-e29b-41d4-a716-446655440000 with seek 5c7a4a{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
           <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Discover you profile
-          </p>
-        </Link>
-      </div>
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <Link href="/games">
-          <h2 className="mb-3 text-2xl font-semibold">
-            Games{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Discover you profile
+            Open the game!
           </p>
         </Link>
       </div>
