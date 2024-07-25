33d9004637db80883b97548247ecbc5c5469a59b
aaglietti-itsrizzoli <104371332+aaglietti-itsrizzoli@users.noreply.github.com>
2024-07-25 00:25:53 +0000

    chore(send): wording

--- app/page.tsx
+++ app/page.tsx
@@ -31,7 +31,7 @@ export default function Home() {
             </span>
           </h2>
           <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Discover you profile
+            Discover avaiable games
           </p>
         </Link>
       </div>
